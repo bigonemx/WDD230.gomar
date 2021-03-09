c4d8c2c9a6a0bffda11585c2517b1c486f4adef0
bigonemx <edwin@gomar.me>
2021-03-09 16:58:32 -0700

    week8

---
+++ lesson8/js/updatedate.js
@@ -0,0 +1,2 @@
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+ document.getElementById('Modified').textContent = new Date().toLocaleDateString('en-US', options);
