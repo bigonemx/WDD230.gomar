bbfeb500376f4ef97ec9912d6163d7a89f160ba1
bigonemx <edwin@gomar.me>
2021-03-30 23:54:21 -0600

    week12

---
+++ lesson11/js/fishheaven.js
@@ -0,0 +1,68 @@
+const apiURL = "//api.openweathermap.org/data/2.5/weather?id=5585000&appid=994a7d4fb87b563dad7d75913d018070&units=imperial"
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((weatherInfo) => {
+    console.log(weatherInfo);
+    document.getElementById("townname").textContent = weatherInfo.name;
+    document.getElementById("current-temp").textContent = weatherInfo.main.temp;
+    document.getElementById("tempmax").textContent = weatherInfo.main.temp_max;
+    document.getElementById('speed').textContent = weatherInfo.wind.speed;
+    document.getElementById('humidity').textContent = weatherInfo.main.humidity;
+
+    let high = parseFloat(document.getElementById('tempmax').innerHTML);
+    let windspeed = parseFloat(document.getElementById('speed').innerHTML);
+
+    let roundedWindspeed = Math.pow(windspeed, 0.16);
+    let windchill = 35.74 + 0.6215 * high - 35.75 * roundedWindspeed + 0.4275 * high * roundedWindspeed;
+
+    if (high <= 50 && windspeed > 3) {
+
+      document.getElementById('windchill').innerHTML = Math.round(windchill);
+    } else {
+
+      windchill = "No windchill";
+    }
+  });
+
+  const forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5585000&units=imperial&appid=994a7d4fb87b563dad7d75913d018070&units=imperial';
+fetch(forecastURL)
+  .then((response) => response.json())
+  .then((forecastObject) => {
+      console.table(forecastObject);
+      var forecast = forecastObject.list.filter(x => x.dt_txt.includes('18:00:00'));
+      console.table(forecast);
+      const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      for (let day = 0; day < forecast.length; day++) {
+          const d = new Date(forecast[day].dt_txt);
+          const imagesrc = 'https://openweathermap.org/img/wn/' + forecast[day].weather[0].icon + '@2x.png';
+          const desc = forecast[day].weather[0].description;
+          document.getElementById(`dayofweek${day+1}`).textContent = weekdays[d.getDay()];
+          document.getElementById(`forecast${day+1}`).textContent = Math.round(forecast[day].main.temp);
+          document.getElementById(`icon${day+1}`).setAttribute('src', imagesrc);
+          document.getElementById(`icon${day+1}`).setAttribute('alt', desc);
+      }
+  });
+
+
+  const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+ fetch(requestURL)
+ .then(function (response) {
+     return response.json();
+ })
+ .then(function (jsonObject) {
+     const towns = jsonObject["towns"];
+     for (let i = 0; i < towns.length; i++) {
+         if (towns[i].name == "Fish Haven") {
+ 
+            let eventBox = document.createElement("div");
+            eventBox.classList.add("events");
+            for (let x = 0; x < towns[i].events.length; x++) {
+                let event = document.createElement("p");
+                event.textContent = towns[i].events[x];
+                eventBox.appendChild(event);
+                document.getElementById("box").appendChild(eventBox);
+            }
+ 
+         }
+     }
+ })
--- lesson11/js/prestonapi.js
+++ lesson11/js/prestonapi.js
@@ -42,3 +42,27 @@ fetch(forecastURL)
           document.getElementById(`icon${day+1}`).setAttribute('alt', desc);
       }
   });
+
+
+  const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+ fetch(requestURL)
+ .then(function (response) {
+     return response.json();
+ })
+ .then(function (jsonObject) {
+     const towns = jsonObject["towns"];
+     for (let i = 0; i < towns.length; i++) {
+         if (towns[i].name == "Preston") {
+ 
+            let eventBox = document.createElement("div");
+            eventBox.classList.add("events");
+            for (let x = 0; x < towns[i].events.length; x++) {
+                let event = document.createElement("p");
+                event.textContent = towns[i].events[x];
+                eventBox.appendChild(event);
+                document.getElementById("box").appendChild(eventBox);
+            }
+ 
+         }
+     }
+ })
