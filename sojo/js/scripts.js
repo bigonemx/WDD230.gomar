46512beeed3d7c47b815ee9f3629991148de7a80
bigonemx <edwin@gomar.me>
2021-04-10 16:29:44 -0600

    sojo

--- sojo/js/scripts.js
+++ sojo/js/scripts.js
@@ -36,12 +36,38 @@ fetch(forecastURL)
       }
   });
 
-
-
-
-
-
-
 // update info (footer)  
 const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
 document.getElementById('Modified').textContent = new Date().toLocaleDateString('en-US', options);
+
+
+//Lazyload
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+
+if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    });
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
+
+
+
+
